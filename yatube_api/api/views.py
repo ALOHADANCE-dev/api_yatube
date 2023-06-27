9a93ea09a7843d63e402ef0275aa29333d1f79f9
Данила Удовиченко <danyarusskiy17@mail.ru>
2023-06-27 17:27:55 +0400

    наконец то прошел все тесты, первая сдача

---
+++ yatube_api/api/views.py
@@ -0,0 +1,36 @@
+from rest_framework import viewsets
+from rest_framework import permissions
+from django.shortcuts import get_object_or_404
+
+from .permission import IsAuthorOrReadOnly
+from posts.models import Post, Group, Comment
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated, IsAuthorOrReadOnly)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        new_queryset = Comment.objects.filter(post=post_id)
+        return new_queryset
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
